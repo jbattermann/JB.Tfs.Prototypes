f7109141ddd25aa5bf5c55b09007af7b44536281
Joerg Battermann <jb@joergbattermann.com>
2012-01-28 14:51:21 +0100

    Initial commit

---
+++ JB.Tfs.Prototypes.TestResultCoverage/Program.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.cs" company="Joerg Battermann">
+//     (c) 2012 Joerg Battermann.
+//     License: Microsoft Public License (Ms-PL). For details see https://github.com/jbattermann/JB.Tfs.Prototypes/blob/master/LICENSE
+// </copyright>
+// <author>Joerg Battermann</author>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JB.Tfs.Prototypes.TestResultCoverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
